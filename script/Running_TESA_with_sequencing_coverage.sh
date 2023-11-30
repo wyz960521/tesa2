97170cb202b3713db2dbb84dd0fe9de3b03a0c54
wyz960521 <75603144+wyz960521@users.noreply.github.com>
2023-11-30 22:01:02 +0800

    Update Running_TESA_with_sequencing_coverage.sh

--- script/Running_TESA_with_sequencing_coverage.sh
+++ script/Running_TESA_with_sequencing_coverage.sh
@@ -2,6 +2,12 @@
 
 # Ensure that the current directory is the tesa root directory
 
+# Tesa file compilation
+cd src
+make clean && make
+cd ..
+
+# Running TESA using an input file with sequencing coverage
 chmod +x preprocess.sh
 cd example
 ../preprocess.sh test reference.fa reference.fa.fai test_out
