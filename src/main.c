fa289f201f00c6f919843346058a75acc2ff6bab
wyz960521 <75603144+wyz960521@users.noreply.github.com>
2023-12-12 12:28:12 +0800

    Update main.c

--- src/main.c
+++ src/main.c
@@ -17,7 +17,7 @@ int main(int argc, char *argv[])
 	/* get the program options defined in get_options.c */
 	get_options(argc, argv);
 	/* pop up the information of Bregulon */
-	printf("\nWTSA %.2f: motif finding on genome scale (compiled "__DATE__
+	printf("\nTESA %.2f: motif finding on genome scale (compiled "__DATE__
 				 " "__TIME__
 				 ")\n\n",
 				 VER);
