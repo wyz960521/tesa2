9a8351b9a9611735d2be1e0fcf4d2c9397fc518a
wyz960521 <75603144+wyz960521@users.noreply.github.com>
2023-12-14 19:57:45 +0800

    Update closure_process.c

--- src/closure_process.c
+++ src/closure_process.c
@@ -83,14 +83,16 @@ continuous get_similarity_between_two_patterns(int seq1, int seq2, int pos1, int
 
 	continuous binomial14[] = {
 			0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 2.7, 3.5, 4.4, 5.5, 6.8, 8.4};
-	/*
- for(i=0;i<motif_length;++i){
-	if(pos1+i<MAX_SEQUENCE_LENGTH){
-		num+=fre_matrix[seq2][seq_matrix[seq1][pos1+i]][pos2+i];
+	
+ 	for(i=0;i<motif_length;++i)
+  	{
+		if(pos1+i<MAX_SEQUENCE_LENGTH)
+		{
+			num+=fre_matrix[seq2][seq_matrix[seq1][pos1+i]][pos2+i];
+		}
 	}
-}
-*/
-	num = fre_matrix[seq2][seq_matrix[seq1][pos1 + 1]][pos2 + 1] + fre_matrix[seq2][seq_matrix[seq1][pos1 + 2]][pos2 + 2] + fre_matrix[seq2][seq_matrix[seq1][pos1 + 3]][pos2 + 3] + fre_matrix[seq2][seq_matrix[seq1][pos1 + 4]][pos2 + 4] + fre_matrix[seq2][seq_matrix[seq1][pos1 + 5]][pos2 + 5] + fre_matrix[seq2][seq_matrix[seq1][pos1 + 6]][pos2 + 6] + fre_matrix[seq2][seq_matrix[seq1][pos1 + 7]][pos2 + 7] + fre_matrix[seq2][seq_matrix[seq1][pos1 + 8]][pos2 + 8] + fre_matrix[seq2][seq_matrix[seq1][pos1 + 9]][pos2 + 9] + fre_matrix[seq2][seq_matrix[seq1][pos1 + 10]][pos2 + 10] + fre_matrix[seq2][seq_matrix[seq1][pos1 + 11]][pos2 + 11] + fre_matrix[seq2][seq_matrix[seq1][pos1 + 12]][pos2 + 12] + fre_matrix[seq2][seq_matrix[seq1][pos1 + 13]][pos2 + 13] + fre_matrix[seq2][seq_matrix[seq1][pos1 + 14]][pos2 + 14];
+	
+	/*num = fre_matrix[seq2][seq_matrix[seq1][pos1 + 1]][pos2 + 1] + fre_matrix[seq2][seq_matrix[seq1][pos1 + 2]][pos2 + 2] + fre_matrix[seq2][seq_matrix[seq1][pos1 + 3]][pos2 + 3] + fre_matrix[seq2][seq_matrix[seq1][pos1 + 4]][pos2 + 4] + fre_matrix[seq2][seq_matrix[seq1][pos1 + 5]][pos2 + 5] + fre_matrix[seq2][seq_matrix[seq1][pos1 + 6]][pos2 + 6] + fre_matrix[seq2][seq_matrix[seq1][pos1 + 7]][pos2 + 7] + fre_matrix[seq2][seq_matrix[seq1][pos1 + 8]][pos2 + 8] + fre_matrix[seq2][seq_matrix[seq1][pos1 + 9]][pos2 + 9] + fre_matrix[seq2][seq_matrix[seq1][pos1 + 10]][pos2 + 10] + fre_matrix[seq2][seq_matrix[seq1][pos1 + 11]][pos2 + 11] + fre_matrix[seq2][seq_matrix[seq1][pos1 + 12]][pos2 + 12] + fre_matrix[seq2][seq_matrix[seq1][pos1 + 13]][pos2 + 13] + fre_matrix[seq2][seq_matrix[seq1][pos1 + 14]][pos2 + 14];
 
 	if (num < 8)
 	{
@@ -99,7 +101,7 @@ continuous get_similarity_between_two_patterns(int seq1, int seq2, int pos1, int
 	else
 	{
 		return (binomial14[(int)num]);
-	}
+	}*/
 }
 
 /************************************************************************/
